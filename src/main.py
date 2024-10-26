1e1e38da828ae467d6739a0cd39ace25bc361c63
Javier-Gonzalez-Soldado <javier.gonzalez.soldado@gmail.com>
2024-10-26 20:12:02 +0200

    feat: comment sockets

--- src/main.py
+++ src/main.py
@@ -9,9 +9,16 @@ SERVER_PORT = 8000
 
 
 def main():
+    # Creates new socket: AF_INET -> IPv4; SOCK_STREAM -> TCP
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    # Config Socket Level: SOL_SOCKET -> self Socket; SO_REUSEADDR -> reuse addr if reset; 1 -> True
     server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+    # Associate socket to add and port
     server_socket.bind((SERVER_HOST, SERVER_PORT))
+
+    # Listen incoming conns: 1 -> size of the buffer.
     server_socket.listen(1)
     print(f"Listening on port...{SERVER_PORT}")
 
